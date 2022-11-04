e8f6abbbdcb0aaceeaa9c992bccc522cab828e2b
Jlion-xiehualiang <supper@9126.org>
2022-11-04 17:59:19 +0800

    1

--- example/QuartzDemoService/Jobs/AddJob.cs
+++ example/QuartzDemoService/Jobs/AddJob.cs
@@ -31,6 +31,7 @@ namespace QuartzDemoService
             if (_count > 0)
             {
                 return;
+
             }
 
             //动态注入创建Job ,这里可以通过界面管理方式，通过api方式进行动态创建Job ，并持久化数据库中
